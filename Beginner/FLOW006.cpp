2165b8be9b66ce55b13c4ebcfff3ab69c895c7f2
Nalin Shani <nalinshani14@gmail.com>
2016-11-24 19:14:48 +0530

    Update FLOW006.cpp

--- Beginner/FLOW006.cpp
+++ Beginner/FLOW006.cpp
@@ -2,12 +2,21 @@
 using namespace std;
 int main()
 {
-    int T,i,j;
+    int T,i,j,digit;
     cin>>T;
     int sum[T];
-    float N[T];
+    int N;
     for(i=0;i<T;i++)
     {
-
+        cin>>N;
+        sum[i]=0;
+        while(N!=0)
+        {
+            digit=N%10;
+            N/=10;
+            sum[i]+=digit;
+        }
     }
+    for(i=0;i<T;i++)
+        cout<<sum[i]<<endl;
 }
